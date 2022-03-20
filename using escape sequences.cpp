c017e46c4201ee0585b07d35183f7c3d7cfbda39
Therubikboy13 <101734268+Therubikboy13@users.noreply.github.com>
2022-03-19 20:27:12 -0700

    Add files via upload

---
+++ using escape sequences.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+
+
+int main()
+{
+   char newline = '\n';
+   cout newline;
+   cout << "\" We\ 'll make our escape in sequence\" he said.";
+   cout << "\n\tTheprogram\ 's over, it\'s time to make a beep beep.\a\a";
+   cout newline;
+   return 0;
+}
